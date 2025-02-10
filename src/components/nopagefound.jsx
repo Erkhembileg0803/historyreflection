76d8d54f9f019b11a55ce364c52c5a4a033a49d5
Erkhembileg <b.erkhembileg0803@gmail.com>
2025-02-10 14:06:59 +0800

    Updates

--- src/components/nopagefound.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/nopagefound.css'
-export default function NoPageFound(){
-    return <div className="nopagefound">
-        <div className="text">
-        <h1>404</h1>
-        <p>seems like this link doesn't work</p>
-        </div>
-    </div>
-}
